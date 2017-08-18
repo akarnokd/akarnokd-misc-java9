e1f0e29694749a006df30057fa14cd0549adfbe0
akarnokd <akarnokd@gmail.com>
2017-08-19 01:08:26 +0200

    Update scrabble

--- src/hu/akarnokd/java9/benchmark/ShakespearePlaysScrabbleFlowOpt.java
+++ src/hu/akarnokd/java9/benchmark/ShakespearePlaysScrabbleFlowOpt.java
@@ -4,12 +4,13 @@ import akka.actor.*;
 import hu.akarnokd.java9.flow.FlowAPI;
 import hu.akarnokd.java9.flow.FlowAPIPlugins;
 import hu.akarnokd.java9.flow.functionals.FlowFunction;
+import hu.akarnokd.rxjava2.math.MathFlowable;
+import hu.akarnokd.rxjava2.string.StringFlowable;
+import io.reactivex.*;
+import io.reactivex.functions.Function;
 
 import java.util.*;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Function;
 
 public class ShakespearePlaysScrabbleFlowOpt extends ShakespearePlaysScrabblePerfBase {
 
@@ -143,7 +144,163 @@ public class ShakespearePlaysScrabbleFlowOpt extends ShakespearePlaysScrabblePer
         return finalList2;
     }
 
+    static Flowable<Integer> chars(String word) {
+//        return Flowable.range(0, word.length()).map(i -> (int)word.charAt(i));
+        return StringFlowable.characters(word);
+    }
+
+
+    static Object bench2() throws Exception {
+        //  to compute the score of a given word
+        Function<Integer, Integer> scoreOfALetter = letter -> letterScores[letter - 'a'];
+
+        // score of the same letters in a word
+        Function<Map.Entry<Integer, MutableLong>, Integer> letterScore =
+                entry ->
+                        letterScores[entry.getKey() - 'a'] *
+                                Integer.min(
+                                        (int)entry.getValue().get(),
+                                        scrabbleAvailableLetters[entry.getKey() - 'a']
+                                )
+                ;
+
+
+        Function<String, Flowable<Integer>> toIntegerFlowable =
+                string -> chars(string);
+
+        // Histogram of the letters in a given word
+        Function<String, Single<HashMap<Integer, MutableLong>>> histoOfLetters =
+                word -> toIntegerFlowable.apply(word)
+                        .collect(
+                                () -> new HashMap<>(),
+                                (HashMap<Integer, MutableLong> map, Integer value) ->
+                                {
+                                    MutableLong newValue = map.get(value) ;
+                                    if (newValue == null) {
+                                        newValue = new MutableLong();
+                                        map.put(value, newValue);
+                                    }
+                                    newValue.incAndSet();
+                                }
+
+                        ) ;
+
+        // number of blanks for a given letter
+        Function<Map.Entry<Integer, MutableLong>, Long> blank =
+                entry ->
+                        Long.max(
+                                0L,
+                                entry.getValue().get() -
+                                        scrabbleAvailableLetters[entry.getKey() - 'a']
+                        )
+                ;
+
+        // number of blanks for a given word
+        Function<String, Flowable<Long>> nBlanks =
+                word -> MathFlowable.sumLong(
+                        histoOfLetters.apply(word).flattenAsFlowable(
+                                map -> map.entrySet()
+                        )
+                                .map(blank)
+                )
+                ;
+
+
+        // can a word be written with 2 blanks?
+        Function<String, Flowable<Boolean>> checkBlanks =
+                word -> nBlanks.apply(word)
+                        .map(l -> l <= 2L) ;
+
+        // score taking blanks into account letterScore1
+        Function<String, Flowable<Integer>> score2 =
+                word -> MathFlowable.sumInt(
+                        histoOfLetters.apply(word).flattenAsFlowable(
+                                map -> map.entrySet()
+                        )
+                                .map(letterScore)
+                ) ;
+
+        // Placing the word on the board
+        // Building the streams of first and last letters
+        Function<String, Flowable<Integer>> first3 =
+                word -> chars(word).take(3) ;
+        Function<String, Flowable<Integer>> last3 =
+                word -> chars(word).skip(3) ;
+
+
+        // Stream to be maxed
+        Function<String, Flowable<Integer>> toBeMaxed =
+                word -> Flowable.concat(first3.apply(word), last3.apply(word))
+                ;
+
+        // Bonus for double letter
+        Function<String, Flowable<Integer>> bonusForDoubleLetter =
+                word -> MathFlowable.max(toBeMaxed.apply(word)
+                        .map(scoreOfALetter)
+                ) ;
+
+        // score of the word put on the board
+        Function<String, Flowable<Integer>> score3 =
+                word ->
+//                MathFlowable.sumInt(Flowable.concat(
+//                        score2.apply(word).map(v -> v * 2),
+//                        bonusForDoubleLetter.apply(word).map(v -> v * 2),
+//                        Flowable.just(word.length() == 7 ? 50 : 0)
+//                  ));
+                        MathFlowable.sumInt(Flowable.concat(
+                                score2.apply(word),
+                                bonusForDoubleLetter.apply(word)
+                        )).map(v -> v * 2 + (word.length() == 7 ? 50 : 0))
+//                new FlowableSumIntArray<Integer>(
+//                        score2.apply(word),
+//                        bonusForDoubleLetter.apply(word)
+//                ).map(v -> 2 * v + (word.length() == 7 ? 50 : 0))
+                ;
+
+        Function<Function<String, Flowable<Integer>>, Single<TreeMap<Integer, List<String>>>> buildHistoOnScore =
+                score -> Flowable.fromIterable(shakespeareWords)
+                        .filter(scrabbleWords::contains)
+                        .filter(word -> checkBlanks.apply(word).blockingFirst())
+                        .collect(
+                                () -> new TreeMap<Integer, List<String>>(Comparator.reverseOrder()),
+                                (TreeMap<Integer, List<String>> map, String word) -> {
+                                    Integer key = score.apply(word).blockingFirst() ;
+                                    List<String> list = map.get(key) ;
+                                    if (list == null) {
+                                        list = new ArrayList<>() ;
+                                        map.put(key, list) ;
+                                    }
+                                    list.add(word) ;
+                                }
+                        ) ;
+
+        // best key / value pairs
+        List<Map.Entry<Integer, List<String>>> finalList2 =
+                buildHistoOnScore.apply(score3).flattenAsFlowable(
+                        map -> map.entrySet()
+                )
+                        .take(3)
+                        .collect(
+                                () -> new ArrayList<Map.Entry<Integer, List<String>>>(),
+                                (list, entry) -> {
+                                    list.add(entry) ;
+                                }
+                        )
+                        .blockingGet() ;
+
+
+//        System.out.println(finalList2);
+
+        return finalList2 ;
+    }
+
     public static void main(String[] args) throws Exception {
+
+        System.out.println(bench2());
+        benchmark("ShakespearePlaysScrabbleFlowableOpt", () -> {
+
+            return bench2();
+        });
 
 
         FlowAPIPlugins.executor = Runnable::run;
@@ -177,7 +334,7 @@ public class ShakespearePlaysScrabbleFlowOpt extends ShakespearePlaysScrabblePer
             actors[i] = actorSystem.actorOf(Props.create(ActorExecutor.class));
         }
 
-        FlowAPIPlugins.onExecutor = new Function<Executor, Executor>() {
+        FlowAPIPlugins.onExecutor = new java.util.function.Function<Executor, Executor>() {
             int i;
             @Override
             public Executor apply(Executor e) {
