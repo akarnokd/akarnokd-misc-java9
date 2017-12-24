6ac2f8012a129839e827d8a465255b54181c5bf2
akarnokd <akarnokd@gmail.com>
2017-12-24 10:42:43 +0100

    More async operators

--- src/main/java/hu/akarnokd/java9/asyncenum/AsyncEnumerable.java
+++ src/main/java/hu/akarnokd/java9/asyncenum/AsyncEnumerable.java
@@ -18,16 +18,44 @@ public interface AsyncEnumerable<T> {
         }
     });
 
+    // -------------------------------------------------------------------------------------
+    // Static factories
     static AsyncEnumerable<Integer> range(int start, int count) {
         return new AsyncRange(start, count);
     }
 
-    default CompletionStage<Boolean> forEach(Consumer<? super T> consumer) {
-        return AsyncForEach.forEach(enumerator(), consumer);
+    static <T> AsyncEnumerable<T> empty() {
+        return AsyncEmpty.instance();
     }
+
+    static <T> AsyncEnumerable<T> fromArray(T... array) {
+        return new AsyncFromArray<>(array);
+    }
+
+    static <T> AsyncEnumerable<T> fromIterable(Iterable<T> iterable) {
+        return new AsyncFromIterable<>(iterable);
+    }
+
+    // -------------------------------------------------------------------------------------
+    // Instance transformations
 
     default <R> AsyncEnumerable<R> flatMap(
             Function<? super T, ? extends AsyncEnumerable<? extends R>> mapper) {
         return new AsyncFlatMap<>(this, mapper);
+    }
+
+    default AsyncEnumerable<T> take(long n) {
+        return new AsyncTake<>(this, n);
+    }
+
+    default AsyncEnumerable<T> skip(long n) {
+        return new AsyncSkip<>(this, n);
+    }
+
+    // -------------------------------------------------------------------------------------
+    // Instance consumers
+
+    default CompletionStage<Boolean> forEach(Consumer<? super T> consumer) {
+        return AsyncForEach.forEach(enumerator(), consumer);
     }
 }
